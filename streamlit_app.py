907089650bd2717467c32587a102f82fb791f488
razib-malik <razibul.malik@gmail.com>
2025-08-06 14:55:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def enrich_ip(ip):
 # Load data (replace with your own log file)
 @st.cache_data
 def load_data():
-    data = pd.read_csv("sample_weblogs_enriched.csv")
+    data = pd.read_csv("sample_weblogs1.csv")
     data["timestamp"] = pd.to_datetime(data["timestamp"])
     return data
 
